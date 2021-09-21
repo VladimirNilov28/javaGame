a33bd2636fb723e921f114c945e7795b43884e7a
Vladimir <Vladimir@mail.ru>
2021-09-21 15:14:35 +0300

    ver1.3 nado dodelat

--- src/javagame/javaGame.java
+++ src/javagame/javaGame.java
@@ -21,25 +21,29 @@ public class javaGame {
         //Программа загадывает число и просит пользователя отгадать
         //Если пользователь отгадал, он выйграл
         //Иначе пользователь, проиграл
-        int max = 5, min = 0, attempt = 1, userNum = -1;
-        Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
-        int gameNum = random.nextInt(max - min + 1) + min;
-        System.out.println("Программа задача числа в диапазоне от 0 до 5, угадай");
-        for(int i=0;i<3; i++){
-            userNum = scanner.nextInt();
-            if(gameNum == userNum){
-                System.out.println("Ура! Ты выйграл!");
-                break;
-            }else{
-                if(attempt < 3) {
-                    System.out.println("Неправильно, попробуй еще раз");
-                }else{
-                System.out.println("Ты проиграл. Задумано число: " + gameNum);
+        int choise = 0;
+        while(choise == 0){
+            do{
+                int max = 5, min = 0, attempt = 1, userNum = -1;
+                Random random = new Random();
+                Scanner scanner = new Scanner(System.in);
+                int gameNum = random.nextInt(max - min + 1) + min;
+                System.out.println("Программа задача числа в диапазоне от 0 до 5, угадай");
+                for(int i=0;i<3; i++){
+                    userNum = scanner.nextInt();
+                    if(gameNum == userNum){
+                        System.out.println("Ура! Ты выйграл!");
+                        break;
+                    }else{
+                       if(attempt < 3) {
+                           System.out.println("Неправильно, попробуй еще раз");
+                      }else{
+                       System.out.println("Ты проиграл. Задумано число: " + gameNum);
+                   }
+                }
+                attempt++;
             }
-        }
-        attempt++;
+        }    
     }
-}
 }
    
